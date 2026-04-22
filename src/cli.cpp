6a593c18d5fa14f41f8e6454a0ec3e4e5b3473ec
replit-user <umyashinderu@gmail.com>
2026-04-22 10:38:36 -0600

    Add files via upload

--- src/cli.cpp
+++ src/cli.cpp
@@ -8,7 +8,7 @@ std::string get_version() {
 void show_help() {
     std::cout << "Commands:\n"
               << "  new <path>      - Creates new project\n"
-              << "  build <version> - Builds project and updates version\n"
+              << "  build {version} - Builds project and updates version if a new version was provided\n"
               << "  install <path>  - Installs headers from path\n"
               << "  update          - Updates the tool to the latest version\n"
               << "  clean           - Removes build directory\n"
